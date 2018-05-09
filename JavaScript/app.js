383305a6fc1dcecbe86db13cf4dca925333d81f7
Anshu Kansakar <anshubtechinit@gmail.com>
2018-05-09 20:14:41 +0530

    learning basic

---
+++ JavaScript/app.js
@@ -0,0 +1,20 @@
+console.log("Hi");
+var anshuHeight = 170;
+var anshuAge = 28;
+
+var ganeshHeight = 176;
+var ganeshAge=28;
+
+var anshuScore = anshuHeight + (5 * anshuAge);
+var ganeshScore = ganeshHeight + (5 * ganeshAge);
+
+console.log("He");
+alert("hhhh");
+
+if(anshuScore === ganeshScore){
+    console.log("Match is draw");
+}else if(anshuScore > ganeshScore){
+    console.log("Anshu win with score: "+anshuScore);
+}else {
+    console.log("Ganesh win with score: "+gane);
+}
